78dbdc8f13f951582359902e488bcb644ad222e8
Thomas Wolter <thwolter@gmail.com>
2024-11-17 15:11:08 +0100

    Include data into context check

--- app/services/models.py
+++ app/services/models.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, Field
+from datetime import datetime
 
+today = datetime.now().strftime("%Y-%m-%d")
 
 class RiskDefinitionCheckResponse(BaseModel):
     is_valid: bool = Field(..., description='Whether the text is valid or not.')
@@ -60,11 +62,13 @@ class CheckProjectContextResponse(BaseModel):
     explanation: str = Field(..., description='Explanation of the classification.')
     missing: list[str] = Field(..., description='The list of missing elements in the project context.')
     context_example: str = Field(..., description='An example of a valid project context.')
-    language: str = Field(..., description='The language of the project context.')
+    language: str = Field(..., description='The language of the project context as two-letter ISO 639-1 language code.')
     capabilities: list[str] = Field(..., description='The list of capabilities required for the project.')
     challenges: list[str] = Field(..., description='The list of challenges faced by the project.')
-    budget: str = Field(..., description='The budget of the project.')
+    budget: str = Field(..., description='The budget of the project as number.')
+    currency: str = Field(..., description='The currency of the project budget.')
     timeline: str = Field(..., description='The timeline of the project.')
+    deadline: str = Field(..., description=f'The deadline of the project based on today\'s date being {today}.')
 
 
 class ProjectSummaryResponse(BaseModel):
