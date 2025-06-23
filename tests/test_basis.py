a5ac732197c606f409a9f4889d6167f0a4bf94d1
Thomas Wolter <thwolter@gmail.com>
2025-06-23 13:50:30 +0200

    refactor: rename client parameter to test_client in health check tests

--- tests/test_basis.py
+++ tests/test_basis.py
@@ -1,10 +1,10 @@
-def test_root(client):
-    response = client.get('/api/_health')
+def test_root(test_client):
+    response = test_client.get('/api/_health')
     assert response.status_code == 204
     assert response.content == b''
 
 
-def test_health_check_returns_ok(client):
-    response = client.get('/health-check')
+def test_health_check_returns_ok(test_client):
+    response = test_client.get('/health-check')
     assert response.status_code == 200
     assert response.json() == {'status': 'ok'}
